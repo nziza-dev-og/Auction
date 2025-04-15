b819cfdfc5e76c832cc2297ad2d94129494f48cd
nziza-dev-og <nzizadev@gmail.com>
2025-04-15 13:06:10 +0200

    Update HomePage.tsx
    url

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -89,7 +89,7 @@ export default function HomePage() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="/src/asset/auctionhamer.gif"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy3Zlzt1RGtGOkq2mEDS-Q79-UcUsetdzVmw&s"
             alt="Online auction"
           />
         </div>
